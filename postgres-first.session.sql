fa7548956d4eb9c5c870519c39724f7c2a98cdda
Julia Alekseeva <soifka@gmail.com>
2023-01-18 20:15:29 +0200

    Date types

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,5 +1,12 @@
+DROP TABLE users;
+
 CREATE TABLE users(
     first_name varchar(64),
     last_name varchar(64),
-    email text
+    email text,
+    gender varchar(30),
+    is_subscribed boolean,
+    birthday date,
+    foot_size smallint,
+    height numeric(3, 2)
 );
