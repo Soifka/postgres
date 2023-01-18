901a4e4fb56ed379447443df5b955bd8b0ff4e83
Julia Alekseeva <soifka@gmail.com>
2023-01-18 20:47:31 +0200

    Null check

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,12 +1,19 @@
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name varchar(64),
-    last_name varchar(64),
-    email text,
+    first_name varchar(64) NOT NULL,
+    last_name varchar(64) NOT NULL,
+    email text NOT NULL,
     gender varchar(30),
-    is_subscribed boolean,
+    is_subscribed boolean NOT NULL,
     birthday date,
     foot_size smallint,
     height numeric(3, 2)
 );
+
+INSERT INTO users VALUES
+('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68),
+('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88);
+
+INSERT INTO users VALUES
+(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
