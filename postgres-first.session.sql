0085bef85cbb90452f8e3550380c44d53c7afdd8
Julia Alekseeva <soifka@gmail.com>
2023-01-19 18:39:38 +0200

    Constraints

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,19 +1,24 @@
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name varchar(64) NOT NULL,
-    last_name varchar(64) NOT NULL,
-    email text NOT NULL,
+    first_name varchar(64) NOT NULL CONSTRAINT first_name_not_empty CHECK(first_name != ''),
+    last_name varchar(64) NOT NULL CHECK(last_name != ''),
+    email text NOT NULL CHECK(email != '') UNIQUE,
     gender varchar(30),
     is_subscribed boolean NOT NULL,
     birthday date,
     foot_size smallint,
-    height numeric(3, 2)
+    height numeric(3, 2) CONSTRAINT too_high_user CHECK(height < 3.00)
 );
 
 INSERT INTO users VALUES
 ('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68),
-('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88);
+('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88),
+('Alex', 'Brok', 'mail@google.com', 'male', true, '1984-05-13', 43, 3.80);
+
 
 INSERT INTO users VALUES
 (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO users VALUES
+('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68);
