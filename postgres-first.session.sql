5d4ee6dd0db7cd03205ee3456272c0099424a463
Julia Alekseeva <soifka@gmail.com>
2023-01-19 18:50:18 +0200

    Date check

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -6,19 +6,18 @@ CREATE TABLE users(
     email text NOT NULL CHECK(email != '') UNIQUE,
     gender varchar(30),
     is_subscribed boolean NOT NULL,
-    birthday date,
+    day_of_birth date CHECK(day_of_birth < current_date),
     foot_size smallint,
     height numeric(3, 2) CONSTRAINT too_high_user CHECK(height < 3.00)
 );
 
 INSERT INTO users VALUES
 ('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68),
-('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88),
-('Alex', 'Brok', 'mail@google.com', 'male', true, '1984-05-13', 43, 3.80);
+('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88);
 
 
 INSERT INTO users VALUES
 (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO users VALUES
-('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68);
+('Alex', 'Brok', 'mail@google.com', 'male', true, '2023-05-13', 43, 1.80);
