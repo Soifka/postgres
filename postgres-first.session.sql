fa7ea65e96b13e6c2a90c325ecd7b5f5b2935931
Julia Alekseeva <soifka@gmail.com>
2023-01-19 19:35:30 +0200

    Primary key

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,23 +1,25 @@
-DROP TABLE users;
+DROP TABLE messages;
 
-CREATE TABLE users(
-    first_name varchar(64) NOT NULL CONSTRAINT first_name_not_empty CHECK(first_name != ''),
-    last_name varchar(64) NOT NULL CHECK(last_name != ''),
-    email text NOT NULL CHECK(email != '') UNIQUE,
-    gender varchar(30),
-    is_subscribed boolean NOT NULL,
-    day_of_birth date CHECK(day_of_birth < current_date),
-    foot_size smallint,
-    height numeric(3, 2) CONSTRAINT too_high_user CHECK(height < 3.00)
+CREATE TABLE messages(
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK(body != ''),
+    author varchar(256) NOT NULL CHECK(author != ''),
+    created_at timestamp DEFAULT current_timestamp,
+    is_read boolean DEFAULT false
 );
 
-INSERT INTO users VALUES
-('Ivanna', 'Petrova', 'i.petrova@gmail.com', 'female', true, '1970-09-15', 38, 1.68),
-('Petr', 'Sodorov', 'p.sidorov@gmail.com', 'male', false, '1970-09-15', NULL, 1.88);
+INSERT INTO messages (body, author) VALUES (
+    'How are you?',
+    'Jane'
+);
 
+INSERT INTO messages (author, body) VALUES 
+('Mike', 'I am fine'),
+('Mike', 'I am fine'),
+('Mike', 'I am fine');
 
-INSERT INTO users VALUES
-(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-INSERT INTO users VALUES
-('Alex', 'Brok', 'mail@google.com', 'male', true, '2023-05-13', 43, 1.80);
+INSERT INTO messages VALUES (
+    2,
+    'How are you?',
+    'Jane'
+);
