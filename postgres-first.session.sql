b826f2b2acf2764b6daa2eb5dd721752e3168398
Julia Alekseeva <soifka@gmail.com>
2023-01-19 20:48:57 +0200

    Alter table

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,43 +1,39 @@
-DROP TABLE messages;
+DROP TABLE users;
 
-CREATE TABLE messages(
+CREATE TABLE products(
     id serial PRIMARY KEY,
-    body text NOT NULL CHECK(body != ''),
-    author varchar(256) NOT NULL CHECK(author != ''),
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean DEFAULT false
+    brand varchar(200) NOT NULL CHECK(brand != ''),
+    model varchar(300) NOT NULL CHECK(model != ''),
+    description text, 
+    category varchar(200) NOT NULL CHECK(category != ''),
+    price numeric(10, 2) NOT NULL CHECK(price > 0),
+    discounted_price numeric(10, 2) CHECK(discounted_price < price)
 );
 
-INSERT INTO messages (body, author) VALUES (
-    'How are you?',
-    'Jane'
-);
-
-INSERT INTO messages (author, body) VALUES 
-('Mike', 'I am fine'),
-('Mike', 'I am fine'),
-('Mike', 'I am fine');
-
-INSERT INTO messages VALUES (
-    2,
-    'How are you?',
-    'Jane'
-);
+INSERT INTO products (brand, model, category, price) VALUES 
+('Samsung', 'Galaxy S10', 'smartphone', 2000),
+('Huawei', 'P30', 'smartphone', 5000),
+('iPhone', '14X', 'smartphone', 3000),
+('Asus', 'Vivobook', 'laptop', 10000),
+('HP', '213542 PRO', 'laptop', 9000);
 
 
+INSERT INTO products (brand, model, category, price) VALUES 
+('Samsung', 'Galaxy S10', 'smartphone', 2000),
+('Huawei', 'P30', 'smartphone', 5000);
 
-CREATE TABLE example(
-    x int,
-    y int,
-    PRIMARY KEY (x, y)
-);
 
-insert INTO example VALUES
-(1, 1),
-(1, 3),
-(1, 2),
-(2, 2),
-(2, 4);
+ALTER TABLE products 
+ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand, model);
 
-insert INTO example VALUES
-(1, 1);
+
+ALTER TABLE products
+ADD COLUMN quantity int CHECK(quantity >= 0);
+
+
+ALTER TABLE products 
+ADD CONSTRAINT "product_quantity_check" CHECK(quantity >= 0);
+
+
+ALTER TABLE products 
+DROP CONSTRAINT "product_quantity_check" CHECK(quantity >= 0);
