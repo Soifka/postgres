1200d860890f949dd67b812766f3defae6abac29
Julia Alekseeva <soifka@gmail.com>
2023-01-23 18:55:35 +0200

    1:1

--- postgres-first.session.sql
+++ postgres-first.session.sql
@@ -1,57 +1,14 @@
-DROP TABLE chats;
-
-
-CREATE TABLE messages(
+CREATE TABLE trainers(
     id serial PRIMARY KEY,
-    author_id int REFERENCES members(id),
-    chat_id int REFERENCES chats(id),
-    text text NOT NULL,
-    send_at timestamp NOT NULL DEFAULT current_timestamp,
-    is_read boolean DEFAULT false,
-    FOREIGN KEY(chat_id, member_id)
+    name varchar(256) NOT NULL
 );
 
 
-CREATE TABLE members(
+CREATE TABLE teams(
     id serial PRIMARY KEY,
-    nickname varchar(50) NOT NULL CONSTRAINT "nickname_must_be_specified" CHECK(nickname != '')
+    name varchar(256) NOT NULL,
+    trainer_id int REFERENCES trainers(id)
 );
 
 
-CREATE TABLE chats(
-    id serial PRIMARY KEY,
-    chat_name varchar(100) NOT NULL CONSTRAINT "chat_name_must_be_specified" CHECK(chat_name != ''),
-    owner_id int REFERENCES members(id),
-    started_at timestamp NOT NULL DEFAULT current_timestamp,
-    is_private boolean DEFAULT false
-);
-
-
-CREATE TABLE chats_to_members(
-    chat_id int REFERENCES chats(id),
-    member_id int REFERENCES members(id),
-    PRIMARY KEY(chat_id, member_id)
-);
-
-
-INSERT INTO members (nickname) VALUES
-('John'),
-('Jane'),
-('Anna'),
-('Mike'),
-('Alex')
-;
-
-
-INSERT INTO chats (chat_name, owner_id) VALUES
-('Party on Friday', 3),
-('School chat', 2);
-
-
-INSERT INTO messages (author_id, chat_id, text) VALUES
-(3, 1, 'Hi, everybody! My party will be on Friday'),
-(2, 2, 'Hello');
-
-
-INSERT INTO messages (author_id, chat_id, text) VALUES
-(5, 1, 'Great! I will come');
+ALTER TABLE trainers ADD COLUMN team_id int REFERENCES teams(id);
