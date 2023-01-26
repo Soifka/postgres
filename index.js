5a5753402e6ce78dc67f5ad0b50a57f3ec550c02
Julia Alekseeva <soifka@gmail.com>
2023-01-26 21:00:27 +0200

    Generator (complete with users)

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { generatePhones } = require('./utils');
 async function start() {
     await client.connect();
 
-    // const userArray = await getUsers();
-    // const res = await User.bulkCreate(userArray);
+    const userArray = await getUsers();
+    const res = await User.bulkCreate(userArray);
 
     const { rows: users } = await User.findAll();
     const phones = await Phone.bulkCreate(generatePhones(50));
