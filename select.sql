82d2c154828aae473f918fe11ae8fc68ffb5a06f
Julia Alekseeva <soifka@gmail.com>
2023-01-28 19:30:05 +0200

    Aliases

--- select.sql
+++ select.sql
@@ -1,51 +1,14 @@
--- найти максимальный вес
-SELECT max(weight) FROM users;
+-- Alias ("псевдонимы для колонок/таблиц") --
+-- Псевдоним нужно писать в кавычках, если он не на английском языке; если
+-- на английском, то можно без кавычек
 
--- найти минимальный вес
-SELECT min(weight) FROM users;
+SELECT *, extract(years from age(birthday)) AS age FROM users
+WHERE extract(years from age(birthday)) BETWEEN 20 AND 40;
 
--- найти сумму веса всех юзеров
-SELECT sum(weight) FROM users;
-
--- найти средний вес
-SELECT avg(weight) FROM users;
-
--- найти количество записей в таблице
-SELECT count(id) FROM users;
-
--- найти средний вес по группам пользователей
-SELECT gender, avg(weight) FROM users
-GROUP BY gender;
-
--- средний вес юзеров, которые родились после 1970 года
-SELECT avg(weight) 
-FROM users
-WHERE extract(years from birthday) > 1970;
-
--- средний вес мужчин, которым 27 лет
-SELECT avg(weight) 
-FROM users
-WHERE extract(years from age(birthday)) = 27 AND gender = 'male';
-
--- средний возраст всех пользователей
-SELECT avg(extract(years from age(birthday))) 
+SELECT 
+first_name AS "Имя", 
+last_name AS "Фамилия", 
+id AS "Личный номер" 
 FROM users;
 
--- минимальный и максимальный возраст пользователей
-SELECT min(extract(years from age(birthday))), max(extract(years from age(birthday))) 
-FROM users;
-
--- минимальный и максимальный возраст пользователей (отдельно мужчин и женщин)
-SELECT gender, min(extract(years from age(birthday))), max(extract(years from age(birthday))) 
-FROM users
-GROUP BY gender;
-
--- кол-во пользователей-мужчин
-SELECT count(id)
-FROM users
-WHERE gender = 'male';
-
--- кол-во пользователей (отдельно мужчин и женщин)
-SELECT gender, count(id)
-FROM users
-GROUP BY gender;
+SELECT * FROM orders_to_products AS "Чек"  --> псевдонимы для таблиц используются в подзапросах
