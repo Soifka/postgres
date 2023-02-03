b29f39cde4ef7864bf7846e0939541a8f2f9ecf0
Julia Alekseeva <soifka@gmail.com>
2023-02-03 20:43:15 +0200

    Practice tasks (CASE + JOIN

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -75,3 +75,142 @@ SELECT *, (
     END
 )
 FROM users; 
+
+
+/*
+Вывести юзеров так, 
+чтобы в поле гендер у них было указано "женщина", "мужчина" или "другое"
+*/
+
+--- syntax 2 ---
+SELECT id, first_name, last_name, email, (
+    CASE gender
+        WHEN 'male' THEN 'мужчина'
+        WHEN 'female' THEN 'женщина'
+        ELSE 'другое'
+    END    
+) AS gender
+FROM users
+ORDER BY id;
+
+
+/*
+На основе возраста пользователя 
+вывести в отдельной колонке, совершеннолетний он или нет
+*/
+
+--- syntax 1 ---
+SELECT *, (
+    CASE
+        WHEN extract(years from age(birthday)) >= 18 
+        THEN 'совершеннолетний'
+        WHEN extract(years from age(birthday)) < 18 
+        THEN 'несовершеннолетний'
+        ELSE 'другое'
+    END    
+) AS "Совершеннолетие"
+FROM users
+ORDER BY id;
+
+
+/*
+Вывести пользователей с информацией об их заказах в следующем виде:
+- если у пользователя 3 и более заказов, то он "постоянный клиент"
+- если у пользователя от 1 до 3 заказов - "активный клиент"
+- если 0 заказов, то "новый клиент"
+*/
+
+--- решение с WITH ---
+WITH users_with_orders AS (
+    SELECT u.*, count(*) AS orders_quantity
+    FROM users AS u
+    JOIN orders AS o  --> нужен именно LEFT JOIN
+    ON u.id = o.customer_id
+    GROUP BY u.id
+)
+SELECT *, (
+    CASE
+        WHEN users_with_orders.orders_quantity >= 3
+        THEN 'постоянный клиент'
+        WHEN users_with_orders.orders_quantity < 3 AND users_with_orders.orders_quantity > 0
+        THEN 'активный клиент'
+        ELSE 'новый клиент'
+    END    
+) AS customer_status
+FROM users_with_orders;
+
+
+------------
+SELECT *, (
+    CASE
+        WHEN users_with_orders.orders_quantity >= 3
+        THEN 'постоянный клиент'
+        WHEN users_with_orders.orders_quantity < 3 AND users_with_orders.orders_quantity > 0
+        THEN 'активный клиент'
+        ELSE 'новый клиент'
+    END    
+) AS customer_status
+FROM (
+    SELECT u.*, count(*) AS orders_quantity
+    FROM users AS u
+    JOIN orders AS o  --> нужен именно LEFT JOIN
+    ON u.id = o.customer_id
+    GROUP BY u.id
+) AS users_with_orders;
+
+
+--- mentor's resolve ---
+SELECT u.id, u.first_name, u.last_name, u.email, count(o.id), (
+    CASE
+        WHEN count(o.id) >= 3
+        THEN 'постоянный клиент'
+        WHEN count(o.id) BETWEEN 1 AND 2
+        THEN 'активный клиент'
+        WHEN count(o.id) = 0
+        THEN 'новый клиент'
+        ELSE 'клиент без статуса'
+    END    
+)
+FROM users AS u
+JOIN orders AS o
+ON u.id = o.customer_id
+GROUP BY u.id, u.first_name, u.last_name, u.email;
+
+-------------------------------------------------------------------------
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    gender,
+    is_subscribe,
+    birthday
+  )
+VALUES (
+    'Marsel',
+    'Derby',
+    'marsel@mail.com',
+    'male',
+    true,
+    '2000-01-01'
+);
+
+
+SELECT u.id, u.first_name, u.last_name, u.email, count(o.id), (
+    CASE
+        WHEN count(o.id) >= 3
+        THEN 'постоянный клиент'
+        WHEN count(o.id) BETWEEN 1 AND 2
+        THEN 'активный клиент'
+        WHEN count(o.id) = 0
+        THEN 'новый клиент'
+        ELSE 'клиент без статуса'
+    END    
+)
+FROM users AS u
+LEFT JOIN orders AS o  --> нужен именно LEFT JOIN, чтобы получить и юзеров без заказов
+ON u.id = o.customer_id
+GROUP BY u.id, u.first_name, u.last_name, u.email
+ORDER BY count(o.id);
+
+-------------------------------------------------------------
